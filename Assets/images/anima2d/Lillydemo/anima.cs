fca016926bdc48f3152f3b35da188445ec11406f
xion1041 <xion104@gmail.com>
2020-11-15 08:41:38 +0900

    モーションの設定とライトの設定

---
+++ Assets/images/anima2d/Lillydemo/anima.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class anima : MonoBehaviour
+{
+
+    Animator animator ;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        animator.SetBool("test", false);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            animator.SetBool("test", true);
+        }
+    }
+}
