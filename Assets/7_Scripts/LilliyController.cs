e254b24e5ad708b9b23ae83ec0ca0af4ff8714ae
xion1041 <xion104@gmail.com>
2020-11-23 06:50:26 +0900

    中間コミット

--- Assets/7_Scripts/LilliyController.cs
+++ Assets/7_Scripts/LilliyController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class LilliyController : MonoBehaviour
 {
-    public float speed = 6.0F;       //歩行速度
     public float jumpSpeed = 8.0F;   //ジャンプ力
     public float gravity = 20.0F;    //重力の大きさ
 
@@ -12,8 +11,7 @@ public class LilliyController : MonoBehaviour
     private Vector3 moveDirection = Vector3.zero;
     private float h, v;
     public float move_speed ;
-    public GameObject mesh;
-    public GameObject bone;
+    public GameObject LillyObj;
 
     Animator animator;
     // Use this for initialization
@@ -26,21 +24,17 @@ public class LilliyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        //h = Input.GetAxis("Horizontal");    //左右矢印キーの値(-1.0~1.0)
-        //v = Input.GetAxis("Vertical");      //上下矢印キーの値(-1.0~1.0)
+        
         if (Input.GetKey(KeyCode.A))
         {
             h = -1;
-            mesh.transform.localScale = new Vector3(1, 1, 1);
-            bone.transform.localScale = new Vector3(1, 1, 1);
+            LillyObj.transform.localScale = new Vector3(1, 1, 1);
             animator.SetBool("dash", true);
         }
         else if (Input.GetKey(KeyCode.D))
         {
             h = 1;
-            mesh.transform.localScale = new Vector3(-1, 1, 1);
-            bone.transform.localScale = new Vector3(-1, 1, 1);
+            LillyObj.transform.localScale = new Vector3(-1, 1, 1);
             animator.SetBool("dash", true);
         }
         else
@@ -49,17 +43,22 @@ public class LilliyController : MonoBehaviour
             animator.SetBool("dash", false);
 
         }
-
-        if (controller.isGrounded)
+        moveDirection.x = h * move_speed;
+        if (controller.isGrounded)//地上
         {
-            moveDirection = new Vector3(h * move_speed, 0, v * move_speed);
-            moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= speed;
-            if (Input.GetKey(KeyCode.Space))
+            
+            animator.SetBool("jump", false);
+            if (Input.GetKeyDown(KeyCode.Space))
                 moveDirection.y = jumpSpeed;
+        }
+        else//空中
+        {
+            Debug.Log("空中");
+            animator.SetBool("jump", true);
         }
         moveDirection.y -= gravity * Time.deltaTime;
         controller.Move(moveDirection * Time.deltaTime);
+        
 
     }//Update()
 }
