1d59f9eb34858ec357e1dc3efc4ad140dcc29ce9
ShionTsuchiya <shion-tsuchiya@hyde.co.jp>
2021-01-25 16:43:51 +0900

    攻撃関連をコミット

---
+++ Assets/7_Scripts/LillyAttackController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LillyAttackController : MonoBehaviour
+{
+    //本体だからいらなかった？
+    //public LilliyController LilliyController;
+　　//外には出さないデータ
+    //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+    private CharacterController controller;
+    Animator animator;
+    //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (controller.isGrounded)//地上
+        {
+            if (Input.GetKeyDown("space")){
+
+            }
+        }
+    }
+}
